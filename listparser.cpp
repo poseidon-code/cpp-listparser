eddf8c48730cf7aa84f1762c708fe09140fc5f27
poseidon-code <pritamhalder05062000@gmail.com>
2020-07-19 22:49:31 +0530

    added template types

--- listparser.cpp
+++ listparser.cpp
@@ -1,70 +1,90 @@
 #include<iostream>
 #include<string>
 #include<bits/stdc++.h>
+#include<type_traits>
 
 using namespace std;
 
-class ListParseInt {
+/*
+Go through the README.md on how to use this :  
+*/
+
+
+template <typename T>
+class ListParser {
     private:    string str;             //input String
                 int size;               // LIST size
-                vector <int> list;      // LIST array
+                vector <T> list;        // LIST array
     
-    public:     ListParseInt(string s) {
+    public:     ListParser(string s) {
                     str = s;
                     size = 0;
                     list = create_list();
                 }
-                
+
                 // GENERATES list of <int> type elements
-                vector <int> create_list() {
-                    stringstream ss(str);
-                    vector <int> temp_list;
-                    int x;
-                    while(ss>>x) {
-                        temp_list.push_back(x);
-                        if(ss.peek() == ' ')
-                            ss.ignore();
-                    }
-                    for(int i : temp_list) size++;
-                    return temp_list;
-                }
+                vector <T> create_list();
 
                 // RETURNS Generated List
-                vector <int> get_list() {
-                    return list;
-                }
+                vector <T> get_list();
 
                 // RETURNS Generated List's Size
-                int get_size() {
-                    return size;
-                }
+                int get_size();
 
                 // DISPLAY Generated List
-                void show_list() {
-                    cout<<"[";
-                    for (int i=0; i<size; i++) {
-                        cout<<list[i];
-                        if(i==size-1) break;
-                        else cout<<", ";
-                    }
-                    cout<<"]";
-                }
+                void show_list();
 };
+
+
+template <typename T>
+vector<T> ListParser<T> :: create_list() {
+    stringstream ss(str);
+    vector <T> temp_list;
+    T temp_element;
+    while(ss>>temp_element) {
+        temp_list.push_back(temp_element);
+        if(ss.peek() == ' ') ss.ignore();
+    }
+    for(T i : temp_list) size++;
+    return temp_list;
+}
+
+
+template <typename T>
+vector<T> ListParser<T> :: get_list() {
+    return list;
+}
+
+template <typename T>
+int ListParser<T> :: get_size() {
+    return size;
+}
+
+template <typename T>
+void ListParser<T> :: show_list() {
+    cout<<"[";
+    for (int i=0; i<size; i++) {
+        if(is_same<T, string>::value) 
+            cout<<"\""<<list[i]<<"\"";
+        else if(is_same<T, char>::value) 
+            cout<<"\'"<<list[i]<<"\'";
+        else cout<<list[i];
+        if(i==size-1) break;
+        cout<<", ";
+    }
+    cout<<"]";
+}
+
 
 
 int main() {
     string str; getline(cin, str);      // gets the INPUT STRING from the user
-    ListParseInt ob(str);               // Parse the STRING into ARRAY of <int> type data
+    ListParser<int> ob(str);          // Parse the STRING into ARRAY of <int> type data
 
     // avaialble functions :
-    vector <int> list = ob.get_list();      // gets the PARS'ED' LIST
-    int size = ob.get_size();               // gets the PARS'ED' LIST's SIZE
-    ob.show_list();                         // displays the PARS'ED' LIST
-
-    // simple LIST traversing
-    for (int i=0; i<size; i++) {
-        cout<<list[i]<<" ";
-    }
+    vector<int> list = ob.get_list();   // gets the PARS'ED' LIST
+    int size = ob.get_size();              // gets the PARS'ED' LIST's SIZE
+    ob.show_list();                        // displays the PARS'ED' LIST
 
     return 0;
 }
